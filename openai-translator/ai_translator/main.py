97f4734743ee985d507628bd56daaa58813d7322
Lior Zhao <zhaoli5@oocl.com>
2024-07-23 23:58:56 +0800

    add day2 homework-- the use of ai translator

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     config_loader = ConfigLoader(args.config)
 
     config = config_loader.load_config()
-
     model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
     model = OpenAIModel(model=model_name, api_key=api_key)
@@ -21,7 +20,8 @@ if __name__ == "__main__":
 
     pdf_file_path = args.book if args.book else config['common']['book']
     file_format = args.file_format if args.file_format else config['common']['file_format']
+    target_language = args.target_language if args.target_language else '中文'
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format, target_language)
